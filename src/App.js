d13a0203f22979012df537c62f3b4cf5a13b9f40
Naggenda <naggendajoshua@gmail.com>
2023-01-11 12:54:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Home from './view/Home';
+import Home from './view/Home/home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/view/Home/home.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div className="intro">
+      <div className="profile">
+        <p>joshua</p>
+      </div>
+      <div className="desc">
+        <h2 className="title">Naggenda Joshua</h2>
+        <h2 className="title-job">Software Developer</h2>
+        <div className="desc-para">
+          <p>
+            innovative Software Developer with Expertise in web front-end
+            application development. offering 2 years experience in software
+            development life cycle. keen and quick to learn and master new
+            technologies and work in both team and self directed settings. Feel
+            free to take a look at my latest projects in portfolio section.
+          </p>
+          <button className="btn">More About Me &#8594;</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default home;
