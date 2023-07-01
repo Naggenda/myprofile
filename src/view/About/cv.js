8d06c046106aa088baa201c218c148828bd8fb41
Naggenda <naggendajoshua@gmail.com>
2023-07-01 20:19:36 +0900

    repair

--- src/view/About/cv.js
+++ src/view/About/cv.js
@@ -8,17 +8,17 @@ const cv = () => {
       <div className="cv-details">
         <div className="details">
           <p>First Name: Joshua</p>
-          <p>Age: <span style={{color: "white"}}>23</span></p>
-          <p>Freelance: <span style={{color: "white"}}>Available</span></p>
-          <p>Full Time: <span style={{color: "white"}}>Available</span></p>
-          <p>Phone: <span style={{color: "white"}}>+256 704 58 2023</span></p>
+          <p>Age: <span style={{color: "blue"}}>23</span></p>
+          <p>Freelance: <span style={{color: "blue"}}>Available</span></p>
+          <p>Full Time: <span style={{color: "blue"}}>Available</span></p>
+          <p>Phone: <span style={{color: "blue"}}>+256 704 58 2023</span></p>
         </div>
         <div className="details">
-          <p>Last Name: <span style={{color: "white"}}>Naggenda</span></p>
-          <p>Nationality: <span style={{color: "white"}}>Ugandan</span></p>
-          <p>Address: <span style={{color: "white"}}>Kampala</span></p>
-          <p>Email: <span style={{color: "white"}}>naggendajoshua@gmail.com</span></p>
-          <p>Languages: <span style={{color: "white"}}>English</span></p>
+          <p>Last Name: <span style={{color: "blue"}}>Naggenda</span></p>
+          <p>Nationality: <span style={{color: "blue"}}>Ugandan</span></p>
+          <p>Address: <span style={{color: "blue"}}>Kampala</span></p>
+          <p>Email: <span style={{color: "blue"}}>naggendajoshua@gmail.com</span></p>
+          <p>Languages: <span style={{color: "blue"}}>English</span></p>
         </div>
       </div>
       <Download />
